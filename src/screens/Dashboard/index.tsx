083b5ccb79f51260aa3479f74f7aaeeb29708d0a
Gabriel Simões <gabriel.simoes@MBP-Gabriel-Simoes.local>
2021-08-18 14:46:54 -0300

    termino desafio

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -32,7 +32,9 @@ export function Dashboard() {
      * - call addRepository function sending inputText value;
      * - clean inputText value.
      */
+    addRepository(inputText);
     inputRef.current?.blur();
+    setInputText('');
   }
 
   function handleRepositoryPageNavigation(id: number) {
@@ -44,6 +46,9 @@ export function Dashboard() {
      *  repositoryId: id of the repository
      * })
      */
+     navigate('Repository', {
+        repositoryId: id,
+    });
   }
 
   return (
@@ -62,6 +67,7 @@ export function Dashboard() {
                * changes:
                * onChangeText={YOUR CODE HERE}
                */
+              onChangeText={setInputText}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
               autoCapitalize='none'
@@ -76,6 +82,7 @@ export function Dashboard() {
              * empty (use disabled prop to this):
              * disabled={CONDITION HERE}
              */
+            disabled={!inputText}
             >
               <Icon name="search" size={20} />
             </InputButton>
